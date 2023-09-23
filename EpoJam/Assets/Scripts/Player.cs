113783d214c29f065da95924eac7acd115c21df3
Renan Santos <renandsz@hotmail.com>
2023-09-22 23:17:53 -0300

    player

--- EpoJam/Assets/Scripts/Player.cs
+++ EpoJam/Assets/Scripts/Player.cs
@@ -13,11 +13,14 @@ public class Player : MonoBehaviour
     public float maxMoveTime = 2;
     public bool moving;
 
+    public float speed = 10;
+    public float minSpeed = 3;
     
     // Start is called before the first frame update
     void Start()
     {
         currentMoveTime = maxMoveTime;
+        speed = minSpeed;
     }
 
     // Update is called once per frame
@@ -26,6 +29,7 @@ public class Player : MonoBehaviour
         if (!moving && Input.GetKey(KeyCode.Space))
         {
             angle += Time.deltaTime * anglePerSec ;
+            speed += Time.deltaTime * minSpeed;
             if (angle >= 360) angle -= 360;            
             
             transform.rotation = Quaternion.Euler(0,0,angle);
@@ -34,6 +38,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             lastAngle = angle;
+            speed = minSpeed;
             angle = 0;
 
             moving = true;
@@ -42,12 +47,14 @@ public class Player : MonoBehaviour
         if (moving)
         {
             currentMoveTime -= Time.deltaTime;
+            transform.position += transform.right * (speed * Time.deltaTime);
             if (currentMoveTime <= 0)
             {
                 currentMoveTime = maxMoveTime;
                 moving = false;
+                
             }
-            transform.Translate(transform.right * Time.deltaTime);
+           
         }
         
     }
